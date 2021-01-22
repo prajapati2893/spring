e4f1e1cdb552de1e9fad519d1563efb7b4d93637
prajapati2893 <77622068+prajapati2893@users.noreply.github.com>
2021-01-22 22:40:44 +0530

    Made into Configurable App using Spring

--- src/com/laptop/app/Laptop.java
+++ src/com/laptop/app/Laptop.java
@@ -1,11 +1,15 @@
 package com.laptop.app;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Laptop {
 
 	public static void main(String[] args) {
-		Hardisk hdd = new Toshiba();
-		Ram ram = new Patriot();
-		Cpu cpu = new Intel();
+		ApplicationContext app = new ClassPathXmlApplicationContext("config.xml"); 
+		Hardisk hdd = app.getBean("hardisk", Hardisk.class);
+		Ram ram = app.getBean("ram", Ram.class);
+		Cpu cpu = app.getBean("cpu", Cpu.class);
 		System.out.println("Laptop Specs:");
 		System.out.print("Ram =>");
 		ram.name();
